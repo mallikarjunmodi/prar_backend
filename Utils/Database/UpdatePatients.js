1c0ccf6c4376fa17d5a62a23ac312a5a582599c5
ShashidharBC98 <shashidharbc.1dt17is079@gmail.com>
2024-08-06 19:06:19 +0530

    updated the updatepatients route

--- Utils/Database/UpdatePatients.js
+++ Utils/Database/UpdatePatients.js
@@ -1,24 +1,65 @@
 import dbInstance from "../../DbInstance.js";
+import moment from "moment";
+import { v4 as uuidv4 } from "uuid";
 
 const UpdatePatients = async (patientdata, res) => {
+  const generateNumber = () => {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+    const day = String(currentDate.getDate()).padStart(2, "0");
+    const dateString = `${year}${month}${day}`;
+
+    const randomHex = uuidv4().replace(/-/g, "").substring(0, 16);
+
+    const result = `${dateString}:${randomHex}`;
+
+    return result;
+  };
+
   try {
-    const db = await dbInstance.getLocalDb("users");
-    const usersCollection = db.collection("patients");
-    const result = await usersCollection.insertOne({
+    if (patientdata.birthDate) {
+      patientdata.birthDate = moment(patientdata.birthDate).format(
+        "YYYY-MM-DDTHH:mm:ss.SSSZ"
+      );
+    }
+
+    const db = await dbInstance.getCloudDb("prar_dashboard");
+    const patientsCollection = db.collection("users");
+
+    const result = await patientsCollection.insertOne({
       ...patientdata,
       created_at: new Date(),
       updated_at: new Date(),
       hospital: "Hospital Name Here Example - (Avani)",
+      active: true,
+      bmi: "",
+      uid: "",
+      deceased: false,
+      maritalStatus: "",
+      photo: "https://example.com/profile.jpg",
+      contact: "",
+      // email: "",
+      pwd: "securepassword123",
+      generalPractitioner: "clya8zndt00002e483icon9tn",
+      age: "",
+      userId: generateNumber(),
     });
+
+    const insertedId = result.insertedId;
+
+    console.log("MongoDB UpdatePatients Successful", insertedId);
 
     res.json({
       message: "MongoDB UpdatePatients Successful",
-      patientdata,
+      userId: insertedId,
     });
   } catch (error) {
-    res.json({
+    console.error("An Error Occurred During UpdatePatients", error);
+
+    res.status(500).json({
       message: "An Error Occurred During UpdatePatients",
-      error: true,
+      error: error.message,
     });
   }
 };
